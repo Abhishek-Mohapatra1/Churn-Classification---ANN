0b742e501bd91f38572df8970fa1b8dc7751d07c
Abhishek Mohapatra <103980342+Abhishek-Mohapatra1@users.noreply.github.com>
2025-11-03 12:59:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,16 +6,16 @@ from sklearn.preprocessing import StandardScaler, LabelEncoder, OneHotEncoder
 import pickle
 
 #Load the trained model
-model = tf.keras.models.load_model('Churn classification project/model.h5')
+model = tf.keras.models.load_model('model.h5')
 
 #load encoders and scaler
-with open('Churn classification project/label_encoder_gender.pkl','rb') as file:
+with open('label_encoder_gender.pkl','rb') as file:
     label_encoder_gender= pickle.load(file)
 
-with open('Churn classification project/onehot_encoder_geo.pkl','rb') as file:
+with open('onehot_encoder_geo.pkl','rb') as file:
     onehot_encoder_geo = pickle.load(file)
 
-with open('Churn classification project/scaler.pkl','rb') as file:
+with open('scaler.pkl','rb') as file:
     scaler=pickle.load(file)
 
 
